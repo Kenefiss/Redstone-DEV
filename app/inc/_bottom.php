0939f693c4c9ae104db6a690d90f901340c6034a
Kenefiss <kenefiss@gmail.com>
2022-10-17 17:18:36 +0300

    update two branch

--- app/inc/_bottom.php
+++ app/inc/_bottom.php
@@ -2,7 +2,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 
 
-<script defer src="js/vendors/jquery.min.js"></script>
+<!-- <script defer src="js/vendors/jquery.min.js"></script> -->
 <script defer src="js/vendors/swiper-bundle.min.js"></script>
 <script defer src="js/vendors/gsap.min.js"></script>
 <script defer src="js/app.global.js"></script>
