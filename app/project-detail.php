58265969efc7c1f7948c76b4095a909ab0f85f5e
TarasBronytskyy <tarasbronytskyy@gmail.com>
2022-10-26 14:17:48 +0300

    start project page

---
+++ app/project-detail.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+  <?php include 'inc/_top.php';?>
+  <title>REDSTONE :: Project detail</title>
+</head>
+
+<body>
+  <div id="loader-wrapper"></div>
+  <div id="content-block">
+    <?php include 'inc/_header.php';?>
+
+    <main>
+
+      <!-- BREADCRUMBS -->
+      <div class="section sec-breadcrumbs">
+        <div class="container">
+          <ul class="breadcrumbs">
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <a href="index.php" itemprop="url">
+                <span itemprop="title">Головна</span>
+              </a>
+            </li>
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <a href="portfolio.php" itemprop="url">
+                <span itemprop="title">Портфоліо</span>
+              </a>
+            </li>
+            <li class="active" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <span itemprop="title">Inspe</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- PROJECT BANNER -->
+      <div class="section project-banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-6">
+
+            </div>
+            <div class="col-12 col-lg-6">
+
+            </div>
+          </div>
+        </div>
+        <div class="spacer-xl"></div>
+      </div>
+
+      <!-- OPTIMIZATION -->
+      <div class="section">
+        <div class="container">
+          <div class="row">
+            <div calss="col-12">
+              <div class="title-with-text">
+                <div class="title h2 animate-item fadeInUp">Оптимізовано для кожного пристрою</div>
+              </div>
+            </div>
+          </div>
+          <div class="row animate-item fadeInUp delay-1">
+            <div class="swiper-entry">
+              <div class="swiper-button-prev d-none d-xl-block"><i></i></div>
+              <div class="swiper-button-next d-none d-xl-block"><i></i></div>
+              <div class="swiper-container" data-options='{"spaceBetween": 15, "slidesPerView": 1, "autoHeight": true, "breakpoints": {"1199": {"slidesPerView": 4, "spaceBetween": 60},"991": {"slidesPerView": 3, "spaceBetween": 40},"575": {"slidesPerView": 2, "spaceBetween": 40}}}'>
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-1.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-1.png" type="image/png">
+                        <img src="img/inspe-optimaze-1.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-2.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-2.png" type="image/png">
+                        <img src="img/inspe-optimaze-2.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-3.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-3.png" type="image/png">
+                        <img src="img/inspe-optimaze-3.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-4.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-4.png" type="image/png">
+                        <img src="img/inspe-optimaze-4.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-1.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-1.png" type="image/png">
+                        <img src="img/inspe-optimaze-1.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="optimaze-img">
+                      <picture>
+                        <source srcset="img/inspe-optimaze-2.webp" type="image/webp">
+                        <source srcset="img/inspe-optimaze-2.png" type="image/png">
+                        <img src="img/inspe-optimaze-2.png" alt="">
+                      </picture>
+                    </div>
+                  </div>
+                </div>
+                <div class="swiper-pagination swiper-pagination-relative"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="spacer-xl"></div>
+      </div>
+
+      <!-- WHAT WE USE -->
+      <div class="section">
+        <div class="container">
+          <div class="row">
+            <div calss="col-12">
+              <div class="title-with-text">
+                <div class="title h2 animate-item fadeInUp">Що ми використовуємо для досягнення успіху</div>
+              </div>
+            </div>
+          </div>
+          <div class="row animate-item fadeInUp delay-1">
+            <div class="swiper-entry clients-slider size2">
+              <div class="swiper-container" data-options='{"spaceBetween": 15, "slidesPerView": 4, "autoHeight": true, "slidesPerGroup":4, "breakpoints": {"991": {"slidesPerView": 6},"575": {"slidesPerView": 5}}}'>
+                <div class="swiper-wrapper clients justify-content-xl-center">
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-10.png" loading="lazy" alt=""></div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-11.png" loading="lazy" alt=""></div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-15.png" loading="lazy" alt=""></div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-6.png" loading="lazy" alt=""></div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-7.png" loading="lazy" alt=""></div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="client-img"><img src="img/tech-8.png" loading="lazy" alt=""></div>
+                  </div>
+                </div>
+                <div class="swiper-pagination swiper-pagination-relative"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="spacer-xl"></div>
+      </div>
+
+      <!-- BRIEF -->
+      <?php include 'inc/_brief.php'; ?>
+
+    </main>
+
+    <?php include 'inc/_footer.php';?>
+  </div>
+
+
+  <div class="popup-wrapper" id="popups"></div>
+  <?php include 'inc/_bottom.php';?>
+</body>
+
+</html>
