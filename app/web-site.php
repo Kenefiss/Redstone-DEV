214bc1046bd40e4c3f5832491de262efd2aa8dda
TarasBronytskyy <tarasbronytskyy@gmail.com>
2022-10-26 10:50:56 +0300

    add service page

--- app/web-site.php
+++ app/web-site.php
@@ -29,6 +29,78 @@
         </div>
       </div>
 
+      <!-- SERVICE BANNER WEB SITE -->
+      <div class="section service-banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="banner-content">
+                <h1 class="title h1">Створення<br> сайтів</h1>
+              </div>
+            </div>
+            <div class="col-12 col-lg-6">
+              <div class="service-img text-center">
+                <picture>
+                    <source srcset="img/service-1.webp" type="image/webp">
+                    <source srcset="img/service-1.png" type="image/png">
+                    <img src="img/service-1.png" alt="">
+                  </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-decor">web site</div>
+        <div class="spacer-xl"></div>
+      </div>
+
+      <!-- SERVICE BANNER BRANDING -->
+      <!-- <div class="section service-banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="banner-content">
+                <h1 class="title h1">Брендінг</h1>
+              </div>
+            </div>
+            <div class="col-12 col-lg-6">
+              <div class="service-img text-center">
+                <picture>
+                    <source srcset="img/service-2.webp" type="image/webp">
+                    <source srcset="img/service-2.png" type="image/png">
+                    <img src="img/service-2.png" alt="">
+                  </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-decor">branding</div>
+        <div class="spacer-xl"></div>
+      </div> -->
+
+      <!-- SERVICE BANNER APP -->
+      <!-- <div class="section service-banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="banner-content">
+                <h1 class="title h1">Послуги з проектування<br> мобільних додатків</h1>
+              </div>
+            </div>
+            <div class="col-12 col-lg-6">
+              <div class="service-img text-center">
+                <picture>
+                    <source srcset="img/service-3.webp" type="image/webp">
+                    <source srcset="img/service-3.png" type="image/png">
+                    <img src="img/service-3.png" alt="">
+                  </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="service-decor">mobile app</div>
+        <div class="spacer-xl"></div>
+      </div> -->
+
       <!-- OUR PROCESS AND ITS BENEFITS -->
       <div class="section">
         <div class="container">
@@ -40,7 +112,54 @@
             </div>
           </div>
           <div class="row">
-
+            <div class="col-12 col-xl-6 d-none d-xl-block">
+              <div class="service-imgs">
+                <picture>
+                  <source srcset="img/service-img-1.webp" type="image/webp">
+                  <source srcset="img/service-img-1.png" type="image/png">
+                  <img src="img/service-img-1.png" alt="">
+                </picture>
+                <picture>
+                  <source srcset="img/service-img-2.webp" type="image/webp">
+                  <source srcset="img/service-img-2.png" type="image/png">
+                  <img src="img/service-img-2.png" alt="">
+                </picture>
+              </div>
+            </div>
+            <div class="col-12 col-xl-6">
+              <div class="our-process">
+                <div class="process-item">
+                  <div class="process-number">01</div>
+                  <div class="title h5">Аналіз конкурентів</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+                <div class="process-item">
+                  <div class="process-number">02</div>
+                  <div class="title h5">Унікальний дизайн</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+                <div class="process-item">
+                  <div class="process-number">03</div>
+                  <div class="title h5">Прототипування та перевірка</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+                <div class="process-item">
+                  <div class="process-number">04</div>
+                  <div class="title h5">Розробляємо стратегію</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+                <div class="process-item">
+                  <div class="process-number">05</div>
+                  <div class="title h5">Медіареклама</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+                <div class="process-item">
+                  <div class="process-number">06</div>
+                  <div class="title h5">Аналіз конкурентів</div>
+                  <div class="text">Ми вибираємо прямих і непрямих конкурентів та вивчаємо їх веб-сайтів. Це допомагає нам знаходити нові можливості</div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="spacer-xl"></div>
@@ -57,7 +176,22 @@
             </div>
           </div>
           <div class="row">
-
+            <div class="col-12">
+              <div class="circle-wrapp">
+                <div class="circle-item"><div class="inner-content"><span>Робимо аудит та аналіз</span></div></div>
+                <div class="circle-item"><div class="inner-content"><span>Розробляємо стратегію</span></div></div>
+                <div class="circle-item"><div class="inner-content"><span>Розробка інтерфейсу</span></div></div>
+                <div class="circle-item"><div class="inner-content"><span>Медіареклама</span></div></div>
+                <div class="circle-item"><div class="inner-content"><span>Support</span></div></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-xl-10 offset-xl-1 col-xxl-8 offset-xxl-2">
+              <div class="text text-center">
+                <p>Вивчаємо сайт, знаходимо всі проблемні місця і визначаємо, як їх усунути. Аналізуємо конкурентів в пошуковій видачі та іхні стратегії. Робимо аудит наявного трафіку на сайт у розрізі джерел та ефективності у пошукових системах.</p>
+              </div>
+            </div>
           </div>
         </div>
         <div class="spacer-xl"></div>
@@ -74,8 +208,64 @@
               </div>
             </div>
           </div>
-          <div class="row">
-
+          <div class="projects-wrapp">
+            <div class="row project-item">
+              <div class="col-12 col-lg-5 align-self-center order-2 order-lg-1">
+                <div class="project-info">
+                  <div class="title h4">Finhub</div>
+                  <div class="project-sub-title text upper">Фінансова компанія</div>
+                  <div class="text">Відкрийте для себе - нагородна вебстудія в Україні. Наша українська команда використовує передові веб-дизайни, веб-розробки, брендинг, цифровий маркетинг, електронну комерцію</div>
+                  <a href="#!" class="btn btn-primary">ПЕРЕГЛЯНУТИ РОБОТУ</a>
+                </div>
+              </div>
+              <div class="col-12 col-lg-7 order-1 order-lg-2">
+                <div class="project-img">
+                  <picture>
+                    <source srcset="img/project-1.webp" type="image/webp">
+                    <source srcset="img/project-1.jpg" type="image/jpeg">
+                    <img src="img/project-1.jpg" alt="">
+                  </picture>
+                </div>
+              </div>
+            </div>
+            <div class="row project-item">
+              <div class="col-12 col-lg-5 align-self-center order-2">
+                <div class="project-info">
+                  <div class="title h4">4 Your health</div>
+                  <div class="project-sub-title text upper">система для дієтологів</div>
+                  <div class="text">Відкрийте для себе - нагородна вебстудія в Україні. Наша українська команда використовує передові веб-дизайни, веб-розробки, брендинг, цифровий маркетинг, електронну комерцію</div>
+                  <a href="#!" class="btn btn-primary">ПЕРЕГЛЯНУТИ РОБОТУ</a>
+                </div>
+              </div>
+              <div class="col-12 col-lg-7 order-1">
+                <div class="project-img">
+                  <picture>
+                    <source srcset="img/project-2.webp" type="image/webp">
+                    <source srcset="img/project-2.jpg" type="image/jpeg">
+                    <img src="img/project-2.jpg" alt="">
+                  </picture>
+                </div>
+              </div>
+            </div>
+            <div class="row project-item">
+              <div class="col-12 col-lg-5 align-self-center order-2 order-lg-1">
+                <div class="project-info">
+                  <div class="title h4">INSPE</div>
+                  <div class="project-sub-title text upper">система для стоматологія</div>
+                  <div class="text">Відкрийте для себе - нагородна вебстудія в Україні. Наша українська команда використовує передові веб-дизайни, веб-розробки, брендинг, цифровий маркетинг, електронну комерцію</div>
+                  <a href="#!" class="btn btn-primary">ПЕРЕГЛЯНУТИ РОБОТУ</a>
+                </div>
+              </div>
+              <div class="col-12 col-lg-7 order-1 order-lg-2">
+                <div class="project-img">
+                  <picture>
+                    <source srcset="img/project-3.webp" type="image/webp">
+                    <source srcset="img/project-3.jpg" type="image/jpeg">
+                    <img src="img/project-3.jpg" alt="">
+                  </picture>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="spacer-xl"></div>
@@ -93,9 +283,9 @@
           </div>
           <div class="row">
             <div class="swiper-entry awards-slider">
-              <div class="swiper-button-prev d-none d-xl-block"><i><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 6L9 12L15 18" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" /></svg></i></div>
-              <div class="swiper-button-next d-none d-xl-block"><i><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 18L15 12L9 6" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" /></svg></i></div>
-              <div class="swiper-container" data-options='{"spaceBetween": 15, "slidesPerView": 1, "autoHeight": true, "breakpoints": {"991": {"slidesPerView": 4, "spaceBetween": 50},"575": {"slidesPerView": 2, "spaceBetween": 40}}}'>
+              <div class="swiper-button-prev d-none d-xl-block"><i></i></div>
+              <div class="swiper-button-next d-none d-xl-block"><i></i></div>
+              <div class="swiper-container" data-options='{"spaceBetween": 15, "slidesPerView": 1, "autoHeight": true, "breakpoints": {"1199": {"slidesPerView": 4, "spaceBetween": 50},"991": {"slidesPerView": 3, "spaceBetween": 40},"575": {"slidesPerView": 2, "spaceBetween": 40}}}'>
                 <div class="swiper-wrapper">
                   <div class="swiper-slide award-item">
                     <div class="award-title">Love & Dress</div>
