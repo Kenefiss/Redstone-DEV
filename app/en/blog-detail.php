31ab8b91db8af3661f074f43075924bb96d16974
TarasBronytskyy <tarasbronytskyy@gmail.com>
2023-01-23 10:03:51 +0200

    add blog pages

---
+++ app/en/blog-detail.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+  <?php include 'inc/_top.php';?>
+  <title>REDSTONE :: Blog detail</title>
+</head>
+
+<body>
+  <div id="loader-wrapper"></div>
+  <div id="content-block">
+    <?php include 'inc/_header.php';?>
+
+    <main>
+
+      <!-- BREADCRUMBS -->
+      <div class="section sec-breadcrumbs">
+        <div class="container-fluid">
+          <ul class="breadcrumbs">
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <a href="index.php" itemprop="url">
+                <span itemprop="title">Головна</span>
+              </a>
+            </li>
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <a href="blog.php" itemprop="url">
+                <span itemprop="title">Блог</span>
+              </a>
+            </li>
+            <li class="active" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <span itemprop="title">Тренди веб-дизайну у 2022 році</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- BLOG -->
+      <div class="section">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-xl-10 offset-xl-1 col-xxl-8 offset-xxl-2">
+              <div class="blog-date">20.09.2022</div>
+              <div class="title-with-text full-width">
+                <h1 class="title page-title h2">Тренди веб-дизайну у 2022 році</h1>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-xl-10 offset-xl-1 col-xxl-8 offset-xxl-2">
+              <div class="simple-page text">
+                <h5>Чого чекати від 2020 року в сфері веб-дизайну?</h5>
+                <p>Якісний та потужний веб-дизайн цікавить всіх спеціалістів, які розробляють інтернет-проекти для сучасних компаній України. Рівень якості дизайну впливає безпосередньо на зацікавленість сайтом цільової аудиторії.</p>
+                <img src='img/simple-img-1.jpg' alt=''>
+                <h5>Моушн-дизайн та анімація</h5>
+                <p>По-перше, сайт повинен бути зрозумілим. Будемо відвертими більше половини читачів, які відвідують такі сайти, мало що розуміють у фінансовій сфері. Найчастіше це люди, які стикнулися з проблемою чи питанням</p>
+                <ul>
+                  <li>Кожен рік дизайн трансформується завдяки новим трендам і темам. 2019 рік</li>
+                  <li>Не став виключенням та вніс корективи у сферу веб-дизайну.</li>
+                  <li>Серед популярних трендів веб-дизайну спеціалісти виділяють 3 головних:</li>
+                </ul>
+                <div class="swiper-entry">
+                  <div class="swiper-container" data-options='{"slidesPerView": 1, "effect":"fade", "autoHeight": true, "loop":true, "lazy": true }'>
+                    <div class="swiper-wrapper">
+                      <div class="swiper-slide">
+                        <div class="slide-img">
+                          <picture>
+                            <source srcset="img/simple-img-2.webp" type="image/webp">
+                            <source srcset="img/simple-img-2.jpg" type="image/jpg">
+                            <img src="img/simple-img-2.jpg" alt="" loading="lazy">
+                          </picture>
+                        </div>
+                      </div>
+                      <div class="swiper-slide">
+                        <div class="slide-img">
+                          <picture>
+                            <source srcset="img/simple-img-3.webp" type="image/webp">
+                            <source srcset="img/simple-img-3.jpg" type="image/jpg">
+                            <img src="img/simple-img-3.jpg" alt="" loading="lazy">
+                          </picture>
+                        </div>
+                      </div>
+                      <div class="swiper-slide">
+                        <div class="slide-img">
+                          <picture>
+                            <source srcset="img/simple-img-1.webp" type="image/webp">
+                            <source srcset="img/simple-img-1.jpg" type="image/jpg">
+                            <img src="img/simple-img-1.jpg" alt="" loading="lazy">
+                          </picture>
+                        </div>
+                      </div>
+                      <div class="swiper-slide">
+                        <div class="slide-img">
+                          <picture>
+                            <source srcset="img/simple-img-3.webp" type="image/webp">
+                            <source srcset="img/simple-img-3.jpg" type="image/jpg">
+                            <img src="img/simple-img-3.jpg" alt="" loading="lazy">
+                          </picture>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="swiper-pagination swiper-pagination-relative"></div>
+                  </div>
+                </div>
+                <h5>Сміливі кольори</h5>
+                <p>Сильні кольори були важливою тенденцією протягом останніх двох років і будуть трендом 2020 року. Насичені кольори і навіть надзвичайно яскраві – вони можуть бути первинними або навіть вторинними відтінками.</p>
+                <p>Коли ми ведемо мову про кольорові тенденції, ми бачимо сміливі кольорові поєднання, які стратегічно використовуються для того, щоб веб-дизайни буквально вистрибували з екрану. Веб-дизайн стає все сміливішим і сміливішим, використовуючи світлі неонові і яскраво насичені кольори в поєднанні з більш темними, приглушеними</p>
+                <ol>
+                  <li>Кожен рік дизайн трансформується завдяки новим трендам і темам. 2019 рік</li>
+                  <li>Не став виключенням та вніс корективи у сферу веб-дизайну.</li>
+                  <li>Серед популярних трендів веб-дизайну спеціалісти виділяють 3 головних:</li>
+                </ol>
+                <iframe src="https://www.youtube.com/embed/lmsykQMZ7b0" loading='lazy' allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <blockquote>Сміливі кольори ідеально підходять для ілюстрацій. Вони створюють веселу атмосферу, змушують персонажів виділятися та розповідати історію за зображенням.</blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="spacer-xl"></div>
+      </div>
+
+    </main>
+
+    <?php include 'inc/_footer.php';?>
+  </div>
+
+
+  <div class="popup-wrapper" id="popups"></div>
+  <?php include 'inc/_bottom.php';?>
+</body>
+
+</html>
---
+++ app/en/blog.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+  <?php include 'inc/_top.php';?>
+  <title>REDSTONE :: Blog</title>
+</head>
+
+<body>
+  <div id="loader-wrapper"></div>
+  <div id="content-block">
+    <?php include 'inc/_header.php';?>
+
+    <main>
+
+      <!-- BREADCRUMBS -->
+      <div class="section sec-breadcrumbs">
+        <div class="container-fluid">
+          <ul class="breadcrumbs">
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <a href="index.php" itemprop="url">
+                <span itemprop="title">Головна</span>
+              </a>
+            </li>
+            <li class="active" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+              <span itemprop="title">Блог</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- BLOG -->
+      <div class="section">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="title-with-text">
+                <div class="title page-title h2">Блог</div>
+              </div>
+              <div class="sort-wrapp js-sort-nav">
+                <div class="sort-btn d-xl-none">Всі</div>
+                <nav class="sort-nav">
+                  <ul>
+                    <li><a class="active">Всі</a></li>
+                    <li><a href="#">Корисні поради</a></li>
+                    <li><a href="#">Новини</a></li>
+                    <li><a href="#">Майстер-класи</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div class="row news-wrapp animate-item fadeInUp">
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item1.webp" type="image/webp">
+                    <source srcset="img/news-item1.jpg" type="image/jpeg">
+                    <img src="img/news-item1.jpg" alt="">
+                  </picture>
+                  <span class="date">18.09.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Чотири нагороди для наші роботи в the Awwwards</a>
+              </div>
+            </div>
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item2.webp" type="image/webp">
+                    <source srcset="img/news-item2.jpg" type="image/jpeg">
+                    <img src="img/news-item2.jpg" alt="">
+                  </picture>
+                  <span class="date">10.09.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Безкоштовний майстер-клас «Запуск прибуткового інтернет магазину»</a>
+              </div>
+            </div>
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item3.webp" type="image/webp">
+                    <source srcset="img/news-item3.jpg" type="image/jpeg">
+                    <img src="img/news-item3.jpg" alt="">
+                  </picture>
+                  <span class="date">21.08.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Чотири нагороди для наші роботи в the Awwwards</a>
+              </div>
+            </div>
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item4.webp" type="image/webp">
+                    <source srcset="img/news-item4.jpg" type="image/jpeg">
+                    <img src="img/news-item4.jpg" alt="">
+                  </picture>
+                  <span class="date">01.08.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Безкоштовний майстер-клас «Запуск прибуткового інтернет магазину»</a>
+              </div>
+            </div>
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item5.webp" type="image/webp">
+                    <source srcset="img/news-item5.jpg" type="image/jpeg">
+                    <img src="img/news-item5.jpg" alt="">
+                  </picture>
+                  <span class="date">23.07.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Чотири нагороди для наші роботи в the Awwwards</a>
+              </div>
+            </div>
+            <div class="col-md-6 news-item">
+              <div class="news-img">
+                <a href="blog-detail.php">
+                  <picture>
+                    <source srcset="img/news-item6.webp" type="image/webp">
+                    <source srcset="img/news-item6.jpg" type="image/jpeg">
+                    <img src="img/news-item6.jpg" alt="">
+                  </picture>
+                  <span class="date">20.07.2020</span>
+                </a>
+              </div>
+              <div class="news-title title h5">
+                <a href="blog-detail.php">Як ми зробили редизайн нашого веб-сайту</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="spacer-xl"></div>
+      </div>
+
+      <!-- SEO -->
+      <?php include 'inc/_seo.php'; ?>
+
+    </main>
+
+    <?php include 'inc/_footer.php';?>
+  </div>
+
+
+  <div class="popup-wrapper" id="popups"></div>
+  <?php include 'inc/_bottom.php';?>
+</body>
+
+</html>
